e181e922dd432bdf51fd02ae9e3dbf21ed503911
KrabCode <jakubpodtrzitkorak@gmail.com>
2017-07-24 22:00:17 +0200

    Removed show/hide window, didnt work well enough anyway

--- Program.cs
+++ Program.cs
@@ -285,28 +285,5 @@ namespace BirthdayCommander
             }
 
         }
-
-        //SHOW / HIDE CONSOLE WINDOW
-
-        [DllImport("kernel32.dll")]
-        static extern IntPtr GetConsoleWindow();
-
-        [DllImport("user32.dll")]
-        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
-
-        const int SW_HIDE = 0;
-        const int SW_SHOW = 5;
-
-        public static void HideWindow()
-        {
-            var handle = GetConsoleWindow();
-            ShowWindow(handle, SW_HIDE);
-        }
-
-        public static void ShowWindow()
-        {
-            var handle = GetConsoleWindow();
-            ShowWindow(handle, SW_SHOW);
-        }        
     }
 }
